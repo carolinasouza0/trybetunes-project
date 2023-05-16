562a11cb17ff346f6995f289fc62b664a6fca6cc
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-05-16 18:01:43 -0300

    feat: requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,37 @@ import { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    artistName: '',
+  };
+
+  handleArtistName = ({ target }) => {
+    const { value } = target;
+    this.setState({ artistName: value });
+  };
+
   render() {
+    const minCharacters = 2;
+    const { artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleArtistName }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ artistName.length < minCharacters }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
