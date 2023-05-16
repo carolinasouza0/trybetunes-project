cc7c1ea348fb63f8f97d4c0973caa909f3cced2e
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-05-16 16:05:03 -0300

    feat: requisito 2

---
+++ src/pages/Loading.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="loading">Carregando...</h1>
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,58 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  state = {
+    name: '',
+    loading: false,
+  };
+
+  handleInput = ({ target }) => {
+    const { value } = target;
+    this.setState({ name: value });
+  };
+
+  handleClick = async () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name });
+    this.setState({ loading: false });
+    return history.push('/search');
+  };
+
   render() {
+    const minCharacters = 3;
+    const { name, loading } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              onChange={ this.handleInput }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ name.length < minCharacters }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+          { loading && <Loading />}
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  push: PropTypes.string,
+}.isRequired;
 
 export default Login;
